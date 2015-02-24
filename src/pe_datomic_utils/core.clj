f2a6e7abcf3ee1c3dfac30080245bed68de77294
Paul Evans <evansp2@gmail.com>
2015-02-23 22:24:34 -0500

    Few additions.

--- src/pe_datomic_utils/core.clj
+++ src/pe_datomic_utils/core.clj
@@ -1,6 +1,7 @@
 (ns pe-datomic-utils.core
   (:require [datomic.api :refer [q db] :as d]
             [clj-time.core :as t]
+            [clojure.java.io :refer [resource]]
             [clojure.tools.logging :as log]))
 
 (defn txn-time
@@ -20,3 +21,23 @@
   [conn txnmap]
   (let [tx @(d/transact conn [txnmap])]
     (d/resolve-tempid (d/db conn) (:tempids tx) (:db/id txnmap))))
+
+(defn refresh-db [db-uri schema-files]
+  (do
+    (d/delete-database db-uri)
+    (d/create-database db-uri)
+    (let [conn (d/connect db-uri)]
+      (doseq [schema-file schema-files]
+        @(d/transact conn (read-string (slurp (resource schema-file)))))
+      conn)))
+
+(defn entity-for-parent-by-id
+  [conn parent-entid entity-entid parent-attr]
+  {:pre [(not (nil? parent-entid))
+         (not (nil? entity-entid))]}
+  (let [entity (into {} (d/entity (d/db conn) entity-entid))]
+    ; if entity has no entries (count = 0), then effectively the given
+    ; entity-entid is not associated with any entities in the database
+    (when (and (> (count entity) 0)
+               (= parent-entid (get-in entity [parent-attr :db/id])))
+      [entity-entid (dissoc entity parent-attr)])))
