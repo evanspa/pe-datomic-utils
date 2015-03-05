6b7e16d43ba37d3b056db72e6aaccdbf35e1301f
Paul Evans <evansp2@gmail.com>
2015-03-04 23:06:06 -0500

    Version 0.0.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pe-datomic-utils "0.0.1"
+(defproject pe-datomic-utils "0.0.2-SNAPSHOT"
   :description "A Clojure library of helper functions when working with Datomic."
   :url "https://github.com/evanspa/pe-datomic-utils"
   :license {:name "MIT"
